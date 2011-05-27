f65e49e2cebb7794276ab0c2731d314c893fcd60
Dan Bode <dan@puppetlabs.com>
2011-05-26 18:10:30 -0700

    added more input validation for db user.

--- lib/puppet/type/database_user.rb
+++ lib/puppet/type/database_user.rb
@@ -7,10 +7,11 @@ Puppet::Type.newtype(:database_user) do
   newparam(:name) do
     desc "The name of the user. This uses the 'username@hostname' or username@hosname."
     validate do |value|
+      unless value =~ /\w+@\w+/
+        raise ArgumentError, "Invalid database user #{value}"
+      end
       list = value.split('@')
-      if list.size() != 2
-        raise ArgumentError, "should be one @, is #{list.size()}"
-      elsif list[0].size > 16
+      if list[0].size > 16
         raise ArgumentError,
          "MySQL usernames are limited to a maximum of 16 characters"
       end
