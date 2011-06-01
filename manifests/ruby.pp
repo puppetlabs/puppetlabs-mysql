03d2ad55500dd8938f764dffa6ad1c8100d09e5a
Dan Bode <dan@puppetlabs.com>
2011-05-31 20:46:02 -0700

    Added ensure param to mysql::ruby.
    allows users of the module to specify the 
    package version.

--- manifests/ruby.pp
+++ manifests/ruby.pp
@@ -1,5 +1,6 @@
 #installs the ruby bindings for mysql
 class mysql::ruby(
+  $ensure = installed,
   $package_name = $mysql::params::ruby_package_name 
 ) inherits mysql::params {
   # I am not making the mysql package a dep for this
@@ -13,6 +14,6 @@ class mysql::ruby(
   #  name => $ruby_mysql_name,
     name => $package_name, 
     provider => 'gem',
-    ensure   => installed,
+    ensure   => $ensure,
   }
 }
