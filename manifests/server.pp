b7761efd5b25532c3753997c4dee4a97dd2328f1
Dan Bode <dan@puppetlabs.com>
2011-06-01 20:20:11 -0700

    corrected /etc/my.cnf path

--- manifests/server.pp
+++ manifests/server.pp
@@ -59,7 +59,7 @@ class mysql::server(
     notify    => Exec['mysqld-restart'],
   } 
 
-  file{['/root/.my.cnf', '/etc/mycnf']:
+  file{['/root/.my.cnf', '/etc/my.cnf']:
     owner => 'root',
     group => 'root',
     mode  => '0400',
