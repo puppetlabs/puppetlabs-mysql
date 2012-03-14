0bd3a6bcfab08f2b4a1a0c7bb9fc5c09b655dfe8
Steffen Zieger <me@saz.sh>
2012-03-14 14:55:24 +0100

    add missing privilege create_tablespace_priv

--- lib/puppet/provider/database_grant/mysql.rb
+++ lib/puppet/provider/database_grant/mysql.rb
@@ -9,7 +9,7 @@ MYSQL_USER_PRIVS = [ :select_priv, :insert_priv, :update_priv, :delete_priv,
 	:show_db_priv, :super_priv, :create_tmp_table_priv, :lock_tables_priv,
 	:execute_priv, :repl_slave_priv, :repl_client_priv, :create_view_priv,
 	:show_view_priv, :create_routine_priv, :alter_routine_priv,
-	:create_user_priv, :event_priv, :trigger_priv
+	:create_user_priv, :event_priv, :trigger_priv, :create_tablespace_priv
 ]
 
 MYSQL_DB_PRIVS = [ :select_priv, :insert_priv, :update_priv, :delete_priv,
