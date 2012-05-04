c06f82eef7f99db4ff47300bacad7b4b7e41b0a0
Michael Arnold <github@razorsedge.org>
2012-05-03 19:58:25 -0700

    Update privilege names to comply with 3fbb54de6c.
    Forgot to update the rspec test.

--- spec/classes/mysql_backup_spec.rb
+++ spec/classes/mysql_backup_spec.rb
@@ -12,7 +12,7 @@ describe 'mysql::backup' do
   it { should contain_database_user('testuser@localhost')}
 
   it { should contain_database_grant('testuser@localhost').with(
-    :privileges => [ 'select_priv', 'reload_priv', 'lock_tables_priv' ]
+    :privileges => [ 'Select_priv', 'Reload_priv', 'Lock_tables_priv' ]
   )}
 
   it { should contain_cron('mysql-backup').with(
