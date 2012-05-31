62b395fe6997b5b13ba43190fc9352dee4588579
Branan Purvine-Riley <branan@puppetlabs.com>
2012-05-31 14:15:42 -0700

    Switch to using the puppetlabs_spec_helper gem for common files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,1 @@
-require 'rubygems'
-require 'puppet'
-require 'rspec-puppet'
-
-def param_value(subject, type, title, param)
-  subject.resource(type, title).send(:parameters)[param.to_sym]
-end
-
-Puppet.parse_config
-puppet_module_path = Puppet[:modulepath]
-
-fixture_path = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures'))
-
-RSpec.configure do |c|
-  fixture_module_path = File.join(fixture_path, 'modules')
-  c.module_path = [fixture_module_path, puppet_module_path].join(":")
-  c.manifest_dir = File.join(fixture_path, 'manifests')
-end
+require 'puppetlabs_spec_helper/module_spec_helper'
