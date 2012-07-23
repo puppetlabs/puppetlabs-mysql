5c4f14e3863033f678ddd500932ffc1b2301411d
Branan Purvine-Riley <branan@puppetlabs.com>
2012-07-23 16:39:37 -0700

    Fix negative tests on rspec 2.11

--- spec/classes/mysql_python_spec.rb
+++ spec/classes/mysql_python_spec.rb
@@ -47,9 +47,7 @@ describe 'mysql::python' do
     end
 
     it 'should fail' do
-      expect do
-        subject
-      end.should raise_error(/Unsupported osfamily: foo/)
+      expect { subject }.to raise_error(/Unsupported osfamily: foo/)
     end
   end
 
