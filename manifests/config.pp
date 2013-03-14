629c6ce31c64cee2554bc388b5f105b62017c238
Mikael Fridh <mfridh@marinsoftware.com>
2013-03-14 14:06:42 +0100

    fixes #19744 - no restart on root/.my.cnf change

--- manifests/config.pp
+++ manifests/config.pp
@@ -109,6 +109,7 @@ class mysql::config(
     file { '/root/.my.cnf':
       content => template('mysql/my.cnf.pass.erb'),
       require => Exec['set_mysql_rootpw'],
+      notify  => undef,
     }
 
     if $etc_root_password {
