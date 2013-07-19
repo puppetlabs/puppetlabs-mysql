7c7195ba335f50d7fee0b685edb41d56a7ebce8f
Ashley Penney <ashley.penney@puppetlabs.com>
2013-07-19 15:03:42 -0400

    Refactor MySQL bindings and client packages.
    The current MySQL module is hard to modify, test, and drop in
    replacement components to.  This work starts out by refactoring
    the bindings support in MySQL to a completely seperate bindings
    class in order to reduce the amount of parameters in the main
    class for a feature that is infrequently used.
    
    In addition to this start the movement of client configuration
    and packages to the mysql::client::* namespace.

--- manifests/params.pp
+++ manifests/params.pp
@@ -12,28 +12,35 @@
 #
 class mysql::params {
 
-  $bind_address        = '127.0.0.1'
-  $config_template     = 'mysql/my.cnf.erb'
-  $default_engine      = 'UNSET'
-  $etc_root_password   = false
-  $manage_service      = true
-  $old_root_password   = ''
-  $package_ensure      = 'present'
-  $purge_conf_dir      = false
-  $max_connections     = 151
-  $port                = 3306
-  $max_allowed_packet  = '16M'
-  $root_password       = 'UNSET'
-  $restart             = true
-  $ssl                 = false
-  $key_buffer          = '16M'
-  $thread_stack        = '256K'
-  $thread_cache_size   = 8
-  $myisam_recover      = 'BACKUP'
-  $query_cache_limit   = '1M'
-  $query_cache_size    = '16M'
-  $expire_logs_days    = 10
-  $max_binlog_size     = '100M'
+  $bind_address          = '127.0.0.1'
+  $client_package_ensure = 'present'
+  $config_template       = 'mysql/my.cnf.erb'
+  $default_engine        = 'UNSET'
+  $etc_root_password     = false
+  $expire_logs_days      = 10
+  $key_buffer            = '16M'
+  $manage_service        = true
+  $max_allowed_packet    = '16M'
+  $max_binlog_size       = '100M'
+  $max_connections       = 151
+  $myisam_recover        = 'BACKUP'
+  $old_root_password     = ''
+  $package_ensure        = 'present'
+  $port                  = 3306
+  $purge_conf_dir        = false
+  $query_cache_limit     = '1M'
+  $query_cache_size      = '16M'
+  $restart               = true
+  $root_password         = 'UNSET'
+  $ssl                   = false
+  $thread_cache_size     = 8
+  $thread_stack          = '256K'
+
+  # mysql::bindings
+  $java_package_ensure   = 'present'
+  $perl_package_ensure   = 'present'
+  $python_package_ensure = 'present'
+  $ruby_package_ensure   = 'present'
 
   case $::operatingsystem {
     'Ubuntu': {
@@ -56,82 +63,85 @@ class mysql::params {
       $basedir               = '/usr'
       $config_file           = '/etc/my.cnf'
       $datadir               = '/var/lib/mysql'
-      $tmpdir                = '/tmp'
-      $java_package_name     = 'mysql-connector-java'
       $log_error             = '/var/log/mysqld.log'
-      $perl_package_name     = 'perl-DBD-MySQL'
       $php_package_name      = 'php-mysql'
       $pidfile               = '/var/run/mysqld/mysqld.pid'
-      $python_package_name   = 'MySQL-python'
       $root_group            = 'root'
-      $ruby_package_name     = 'ruby-mysql'
-      $ruby_package_provider = 'gem'
       $service_name          = 'mysqld'
       $socket                = '/var/lib/mysql/mysql.sock'
       $ssl_ca                = '/etc/mysql/cacert.pem'
       $ssl_cert              = '/etc/mysql/server-cert.pem'
       $ssl_key               = '/etc/mysql/server-key.pem'
+      $tmpdir                = '/tmp'
+      # mysql::bindings
+      $java_package_name     = 'mysql-connector-java'
+      $perl_package_name     = 'perl-DBD-MySQL'
+      $python_package_name   = 'MySQL-python'
+      $ruby_package_name     = 'ruby-mysql'
+      $ruby_package_provider = 'gem'
     }
 
     'Suse': {
       $basedir               = '/usr'
-      $datadir               = '/var/lib/mysql'
-      $tmpdir                = '/tmp'
-      $service_name          = 'mysql'
+      $config_file           = '/etc/my.cnf'
       $client_package_name   = $::operatingsystem ? {
         /OpenSuSE/           => 'mysql-community-server-client',
         /(SLES|SLED)/        => 'mysql-client',
         }
-      $server_package_name   = $::operatingsystem ? {
-        /OpenSuSE/           => 'mysql-community-server',
-        /(SLES|SLED)/        => 'mysql',
-        }
-      $socket                = $::operatingsystem ? {
-        /OpenSuSE/           => '/var/run/mysql/mysql.sock',
-        /(SLES|SLED)/        => '/var/lib/mysql/mysql.sock',
-        }
-      $pidfile               = $::operatingsystem ? {
-        /OpenSuSE/           => '/var/run/mysql/mysqld.pid',
-        /(SLES|SLED)/        => '/var/lib/mysql/mysqld.pid',
-        }
-      $config_file           = '/etc/my.cnf'
+      $datadir               = '/var/lib/mysql'
       $log_error             = $::operatingsystem ? {
         /OpenSuSE/           => '/var/log/mysql/mysqld.log',
         /(SLES|SLED)/        => '/var/log/mysqld.log',
-        }
-      $ruby_package_name     = $::operatingsystem ? {
-        /OpenSuSE/           => 'rubygem-mysql',
-        /(SLES|SLED)/        => 'ruby-mysql',
-        }
-      $python_package_name   = 'python-mysql'
-      $perl_package_name     = 'perl-DBD-mysql'
-      $java_package_name     = 'mysql-connector-java'
+      }
+      $pidfile               = $::operatingsystem ? {
+        /OpenSuSE/           => '/var/run/mysql/mysqld.pid',
+        /(SLES|SLED)/        => '/var/lib/mysql/mysqld.pid',
+      }
       $root_group            = 'root'
+      $server_package_name   = $::operatingsystem ? {
+        /OpenSuSE/           => 'mysql-community-server',
+        /(SLES|SLED)/        => 'mysql',
+      }
+      $service_name          = 'mysql'
+      $socket                = $::operatingsystem ? {
+        /OpenSuSE/           => '/var/run/mysql/mysql.sock',
+        /(SLES|SLED)/        => '/var/lib/mysql/mysql.sock',
+      }
       $ssl_ca                = '/etc/mysql/cacert.pem'
       $ssl_cert              = '/etc/mysql/server-cert.pem'
       $ssl_key               = '/etc/mysql/server-key.pem'
+      $tmpdir                = '/tmp'
+      # mysql::bindings
+      $java_package_name     = 'mysql-connector-java'
+      $perl_package_name     = 'perl-DBD-mysql'
+      $python_package_name   = 'python-mysql'
+      $ruby_package_name     = $::operatingsystem ? {
+        /OpenSuSE/           => 'rubygem-mysql',
+        /(SLES|SLED)/        => 'ruby-mysql',
+      }
     }
 
     'Debian': {
       $basedir              = '/usr'
-      $datadir              = '/var/lib/mysql'
-      $tmpdir                = '/tmp'
-      $service_name         = 'mysql'
       $client_package_name  = 'mysql-client'
-      $server_package_name  = 'mysql-server'
-      $socket               = '/var/run/mysqld/mysqld.sock'
-      $pidfile              = '/var/run/mysqld/mysqld.pid'
       $config_file          = '/etc/mysql/my.cnf'
+      $datadir              = '/var/lib/mysql'
       $log_error            = '/var/log/mysql/error.log'
-      $perl_package_name    = 'libdbd-mysql-perl'
-      $ruby_package_name    = 'libmysql-ruby'
-      $python_package_name  = 'python-mysqldb'
       $php_package_name     = 'php5-mysql'
-      $java_package_name    = 'libmysql-java'
+      $pidfile              = '/var/run/mysqld/mysqld.pid'
       $root_group           = 'root'
+      $server_package_name  = 'mysql-server'
+      $service_name         = 'mysql'
+      $socket               = '/var/run/mysqld/mysqld.sock'
       $ssl_ca               = '/etc/mysql/cacert.pem'
       $ssl_cert             = '/etc/mysql/server-cert.pem'
       $ssl_key              = '/etc/mysql/server-key.pem'
+      $tmpdir                = '/tmp'
+      # mysql::bindings
+      $java_package_name    = 'libmysql-java'
+      $perl_package_name    = 'libdbd-mysql-perl'
+      $python_package_name  = 'python-mysqldb'
+      $ruby_package_name    = 'libmysql-ruby'
     }
 
     'FreeBSD': {
@@ -145,41 +155,42 @@ class mysql::params {
       $pidfile               = '/var/db/mysql/mysql.pid'
       $config_file           = '/var/db/mysql/my.cnf'
       $log_error             = "/var/db/mysql/${::hostname}.err"
-      $perl_package_name     = 'p5-DBD-mysql'
-      $ruby_package_name     = 'ruby-mysql'
-      $ruby_package_provider = 'gem'
-      $python_package_name   = 'databases/py-MySQLdb'
       $php_package_name      = 'php5-mysql'
-      $java_package_name     = 'databases/mysql-connector-java'
       $root_group            = 'wheel'
       $ssl_ca                = undef
       $ssl_cert              = undef
       $ssl_key               = undef
+      # mysql::bindings
+      $java_package_name     = 'databases/mysql-connector-java'
+      $perl_package_name     = 'p5-DBD-mysql'
+      $python_package_name   = 'databases/py-MySQLdb'
+      $ruby_package_name     = 'ruby-mysql'
+      $ruby_package_provider = 'gem'
     }
 
     default: {
       case $::operatingsystem {
         'Amazon': {
           $basedir               = '/usr'
-          $datadir               = '/var/lib/mysql'
-          $tmpdir                = '/tmp'
-          $service_name          = 'mysqld'
           $client_package_name   = 'mysql'
-          $server_package_name   = 'mysql-server'
-          $socket                = '/var/lib/mysql/mysql.sock'
           $config_file           = '/etc/my.cnf'
+          $datadir               = '/var/lib/mysql'
           $log_error             = '/var/log/mysqld.log'
-          # XXX validate...
-          $perl_package_name     = 'perl-DBD-MySQL'
-          $ruby_package_name     = 'ruby-mysql'
-          $ruby_package_provider = 'gem'
-          $python_package_name   = 'MySQL-python'
           $php_package_name      = 'php-mysql'
-          $java_package_name     = 'mysql-connector-java'
           $root_group            = 'root'
+          $server_package_name   = 'mysql-server'
+          $service_name          = 'mysqld'
+          $socket                = '/var/lib/mysql/mysql.sock'
           $ssl_ca                = '/etc/mysql/cacert.pem'
           $ssl_cert              = '/etc/mysql/server-cert.pem'
           $ssl_key               = '/etc/mysql/server-key.pem'
+          $tmpdir                = '/tmp'
+          # mysql::bindings
+          $java_package_name     = 'mysql-connector-java'
+          $perl_package_name     = 'perl-DBD-MySQL'
+          $python_package_name   = 'MySQL-python'
+          $ruby_package_name     = 'ruby-mysql'
+          $ruby_package_provider = 'gem'
         }
 
         default: {
--- spec/classes/mysql_bindings_python_spec.rb
+++ spec/classes/mysql_bindings_python_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'mysql::python' do
+describe 'mysql::bindings::python' do
 
   describe 'on a debian based os' do
     let :facts do
