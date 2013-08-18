7d4f9fc6859f8a72e5c8101bb5c11e51d5f3ec58
Ashley Penney <ashley.penney@puppetlabs.com>
2013-08-17 20:46:20 -0400

    Rename and refactor database to mysql_database.
    Add collate as a new managable parameter, and extend self.instances to
    add in all parameters when checking existing databases.  It also adds
    self.prefetch in order to speed up Puppet runs.
    
    Provider is also switched to using mk_resource_methods to generate
    all the resource readers, and exists? and other methods now use the
    property_hash where appropriate.
    
    Tests rewritten to handle changes and extend code coverage.

--- lib/puppet/type/database.rb
+++ lib/puppet/type/database.rb
@@ -1,17 +1,11 @@
-# This has to be a separate type to enable collecting
 Puppet::Type.newtype(:database) do
-  @doc = 'Manage databases.'
 
-  ensurable
-
-  newparam(:name, :namevar=>true) do
-    desc 'The name of the database.'
-  end
-
-  newproperty(:charset) do
-    desc 'The characterset to use for a database'
-    defaultto :utf8
-    newvalue(/^\S+$/)
+  newparam(:name, :namevar => true) do
+    desc 'Manage databases.'
+    validate do |value|
+      Puppet.warning("database has been deprecated in favor of mysql_database.")
+      true
+    end
   end
 
 end
