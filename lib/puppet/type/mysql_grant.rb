ac46da08967409e813ac756f137ec4bd4243d32b
Reinhard Vicinus <rvicinus@laprvicinus.hh.metaways.de>
2013-11-04 12:10:55 +0100

    Revert "bugfix: uppercase all supplied permissions, because provider provides them  	capitalized and "insert" isn't equal to "INSERT""
    This reverts commit 5035572c18576724f27887ece273192a7e0857a9.

--- lib/puppet/type/mysql_grant.rb
+++ lib/puppet/type/mysql_grant.rb
@@ -17,7 +17,7 @@ Puppet::Type.newtype(:mysql_grant) do
     # Sort the privileges array in order to ensure the comparision in the provider
     # self.instances method match.  Otherwise this causes it to keep resetting the
     # privileges.
-    self[:privileges] = Array(self[:privileges]).map(&:upcase).sort!
+    self[:privileges] = Array(self[:privileges]).sort!
   end
 
   validate do
