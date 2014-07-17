4852a03c2ccf479044101f507acdb706280f326e
Stefan Richter <stefan@02strich.de>
2014-07-17 22:16:19 +0200

    Adding CentOS rules

--- manifests/params.pp
+++ manifests/params.pp
@@ -45,6 +45,13 @@ class mysql::params {
             $provider = 'mysql'
           }
         }
+        'CentOS': {
+          if $::operatingsystemmajrelease >= 7 {
+            $provider = 'mariadb'
+          } else {
+            $provider = 'mysql'
+          }
+        }
         default: {
           $provider = 'mysql'
         }
