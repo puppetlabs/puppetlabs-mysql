5b10f3e77379f034934f1b367ce563e95172bf67
Ashley Penney <ashley.penney@puppetlabs.com>
2014-08-08 16:41:23 -0400

    Clean these up to be more readable.

--- spec/classes/mysql_server_account_security_spec.rb
+++ spec/classes/mysql_server_account_security_spec.rb
@@ -6,29 +6,25 @@ describe 'mysql::server::account_security' do
       describe "on #{pe_version} #{pe_platform}" do
         let(:facts) { facts.merge({:fqdn => 'myhost.mydomain', :hostname => 'myhost'}) }
 
-        it 'should remove Mysql_User[root@myhost.mydomain]' do
-          is_expected.to contain_mysql_user('root@myhost.mydomain').with_ensure('absent')
+        [ 'root@myhost.mydomain',
+          'root@127.0.0.1',
+          'root@::1',
+          '@myhost.mydomain',
+          '@localhost',
+          '@%',
+        ].each do |user|
+          it 'removes Mysql_User[#{user}]' do
+            is_expected.to contain_mysql_user(user).with_ensure('absent')
+          end
         end
-        it 'should remove Mysql_User[root@myhost]' do
-          is_expected.to contain_mysql_user('root@myhost').with_ensure('absent')
-        end
-        it 'should remove Mysql_User[root@127.0.0.1]' do
-          is_expected.to contain_mysql_user('root@127.0.0.1').with_ensure('absent')
-        end
-        it 'should remove Mysql_User[root@::1]' do
-          is_expected.to contain_mysql_user('root@::1').with_ensure('absent')
-        end
-        it 'should remove Mysql_User[@myhost.mydomain]' do
-          is_expected.to contain_mysql_user('@myhost.mydomain').with_ensure('absent')
-        end
-        it 'should remove Mysql_User[@myhost]' do
-          is_expected.to contain_mysql_user('@myhost').with_ensure('absent')
-        end
-        it 'should remove Mysql_User[@localhost]' do
-          is_expected.to contain_mysql_user('@localhost').with_ensure('absent')
-        end
-        it 'should remove Mysql_User[@%]' do
-          is_expected.to contain_mysql_user('@%').with_ensure('absent')
+
+        # When the hostname doesn't match the fqdn we also remove these.
+        # We don't need to test the inverse as when they match they are
+        # covered by the above list.
+        [ 'root@myhost', '@myhost' ].each do |user|
+          it 'removes Mysql_User[#{user}]' do
+            is_expected.to contain_mysql_user(user).with_ensure('absent')
+          end
         end
 
         it 'should remove Mysql_database[test]' do
