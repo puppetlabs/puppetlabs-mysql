058053b5bc5f51707e2bccbde133ba944c5accc8
Morgan Haskel <morgan@puppetlabs.com>
2014-08-28 17:49:18 -0400

    Update spec_helper for more consistency

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -3,11 +3,12 @@ require 'beaker-rspec'
 UNSUPPORTED_PLATFORMS = [ 'Windows', 'Solaris', 'AIX' ]
 
 unless ENV['RS_PROVISION'] == 'no' or ENV['BEAKER_provision'] == 'no'
-  if hosts.first.is_pe?
-    install_pe
-  else
-    install_puppet
-  end
+  # This will install the latest available package on el and deb based
+  # systems fail on windows and osx, and install via gem on other *nixes
+  foss_opts = { :default_action => 'gem_install' }
+
+  if default.is_pe?; then install_pe; else install_puppet( foss_opts ); end
+
   hosts.each do |host|
     on hosts, "mkdir -p #{host['distmoduledir']}"
   end
