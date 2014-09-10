d986a875589bb26ce9fb4b958c589d2ba0f08ab8
Matthew Monaco <matt@monaco.cx>
2014-09-10 17:55:45 -0600

    mysql_database: prevent syntax error with collate=>'binary'
    On MySQL v5.5.38, creating a database such as:
    
    	CREATE DATABASE `mydb` CHARACTER SET binary COLLATE binary;
    
    seems to hit a parser bug. A workaround is simply to quote COLLATE
    `binary`. As the quoting is harmless, and for aesthetics, quote both
    the CHARACTER SET and COLLATE arguments.

--- spec/unit/puppet/provider/mysql_database/mysql_spec.rb
+++ spec/unit/puppet/provider/mysql_database/mysql_spec.rb
@@ -56,7 +56,7 @@ test
 
   describe 'create' do
     it 'makes a database' do
-      provider.expects(:mysql).with([defaults_file, '-NBe', "create database if not exists `#{resource[:name]}` character set #{resource[:charset]} collate #{resource[:collate]}"])
+      provider.expects(:mysql).with([defaults_file, '-NBe', "create database if not exists `#{resource[:name]}` character set `#{resource[:charset]}` collate `#{resource[:collate]}`"])
       provider.expects(:exists?).returns(true)
       expect(provider.create).to be_truthy
     end
