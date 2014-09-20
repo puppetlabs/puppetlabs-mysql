5e6a1c418a8cb40a91619afec13e127a16ce16f4
Yanis Guenane <yguenane@gmail.com>
2014-09-20 18:43:01 -0400

    (MODULES-1338) Allow mysql::db to import several files
    A user might need to import several files on database creation.
    Currently the module only allows the import of a single file.
    This commit allows one to, from now on, import severals.
    
    Before :
    
      mysql::db { 'test' :
        sql => '/tmp/my_import1.sql',
      }
    
    Now :
    
      mysql::db { 'test' :
        sql => [
          '/tmp/my_import1.sql',
          '/tmp/my_import2.sql',
        ]
      }

--- manifests/db.pp
+++ manifests/db.pp
@@ -2,20 +2,21 @@
 define mysql::db (
   $user,
   $password,
-  $dbname      = $name,
-  $charset     = 'utf8',
-  $collate     = 'utf8_general_ci',
-  $host        = 'localhost',
-  $grant       = 'ALL',
-  $sql         = undef,
-  $enforce_sql = false,
-  $ensure      = 'present',
+  $dbname         = $name,
+  $charset        = 'utf8',
+  $collate        = 'utf8_general_ci',
+  $host           = 'localhost',
+  $grant          = 'ALL',
+  $sql            = undef,
+  $enforce_sql    = false,
+  $ensure         = 'present',
   $import_timeout = 300,
 ) {
   #input validation
   validate_re($ensure, '^(present|absent)$',
   "${ensure} is not supported for ensure. Allowed values are 'present' and 'absent'.")
   $table = "${dbname}.*"
+  $sql_inputs = join(any2array($sql), ' ')
 
   include '::mysql::client'
 
@@ -49,10 +50,11 @@ define mysql::db (
 
     if $sql {
       exec{ "${dbname}-import":
-        command     => "/usr/bin/mysql ${dbname} < ${sql}",
+        command     => "cat ${sql_inputs} | mysql ${dbname}",
         logoutput   => true,
         environment => "HOME=${::root_home}",
         refreshonly => $refresh,
+        path        => '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin',
         require     => Mysql_grant["${user}@${host}/${table}"],
         subscribe   => Mysql_database[$dbname],
         timeout     => $import_timeout,
