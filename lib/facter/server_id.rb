db48433ec5fd01b8cad7445deaeccb1135aa4d0f
Nick Le Mouton <nick@noodles.net.nz>
2014-11-07 10:08:37 +1300

    Added condition to handle servers with no ipv4 address

--- lib/facter/server_id.rb
+++ lib/facter/server_id.rb
@@ -1,6 +1,12 @@
 def get_mysql_id
     mysql_id = nil;
-    mysql_id = Facter.value(:ipaddress).split('.').inject(0) {|total,value| (total << 8 ) + value.to_i}
+    
+    if Facter.value(:ipaddress) != nil
+      mysql_id = Facter.value(:ipaddress).split('.').inject(0) {|total,value| (total << 8 ) + value.to_i}
+    else
+      mysql_id = Facter.value(:ipaddress6).split(':').inject(0) {|total,value| (total << 2) + value.hex}
+    end
+    
 end
 
 Facter.add("mysql_server_id") do
