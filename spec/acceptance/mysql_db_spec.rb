3036cd40e23e077f3adbed474335650c19f4f976
Joshua Brunner <j.brunner@nexbyte.com>
2014-11-11 13:49:33 +0100

    test: Add test for creating users with empty password using mysql::db
    An empty password should not be hashed.

--- spec/acceptance/mysql_db_spec.rb
+++ spec/acceptance/mysql_db_spec.rb
@@ -68,4 +68,28 @@ describe 'mysql::db define' do
       expect(shell("mysql -e 'show databases;'|grep realdb").exit_code).to be_zero
     end
   end
+
+  describe 'creating a database with empty user password' do
+    # Using puppet_apply as a helper
+    it 'should work with no errors' do
+      pp = <<-EOS
+        class { 'mysql::server': override_options => { 'root_password' => 'password' } }
+        mysql::db { 'spec3':
+          user     => 'root3',
+          password => '',
+        }
+      EOS
+
+      # Run it twice and test for idempotency
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes => true)
+    end
+
+    it 'should have an empty password' do
+      shell("mysql -NBe \"show grants for root3@localhost\"") do |r|
+        expect(r.stdout).not_to match(/IDENTIFIED BY/)
+        expect(r.stderr).to be_empty
+      end
+    end
+  end
 end
