60838a583796e48442430a3af955e4998fc2960c
Sébastien Lavoie <github@lavoie.sl>
2014-11-26 14:46:39 -0500

    Reworked all identifier quoting detections
    Otherwise, trying to manage `debian-sys-maint` will fail miserably, quoted or not.
    
    Fixes #609

--- lib/puppet/type/mysql_user.rb
+++ lib/puppet/type/mysql_user.rb
@@ -10,19 +10,28 @@ Puppet::Type.newtype(:mysql_user) do
     desc "The name of the user. This uses the 'username@hostname' or username@hostname."
     validate do |value|
       # http://dev.mysql.com/doc/refman/5.5/en/identifiers.html
-      # Regex should problably be more like this: /^[`'"]?[^`'"]*[`'"]?@[`'"]?[\w%\.]+[`'"]?$/
       # If at least one special char is used, string must be quoted
-      raise(ArgumentError, "Database user #{value} must be quotted as it contains special characters") if value =~ /^[^'`"].*[^0-9a-zA-Z$_].*[^'`"]@[\w%\.:]+/
-      raise(ArgumentError, "Invalid database user #{value}") unless value =~ /^(?:['`"][^'`"]*['`"]|[0-9a-zA-Z$_]*)@[\w%\.:]+/
-      username = value.split('@')[0]
-      if not ((username =~ /['"`]*['"`]$/ and username.size <= 18) or username.size <= 16)
-        raise ArgumentError, 'MySQL usernames are limited to a maximum of 16 characters'
+
+      # http://stackoverflow.com/questions/8055727/negating-a-backreference-in-regular-expressions/8057827#8057827
+      if matches = /^(['`"])((?:(?!\1).)+)\1@([\w%\.:]+)$/.match(value)
+        user_part = matches[2]
+        host_part = matches[3]
+      elsif matches = /^([0-9a-zA-Z$_]+)@([\w%\.:]+)$/.match(value)
+        user_part = matches[1]
+        host_part = matches[2]
+      elsif matches = /^(?:(?!['`"]).*)([^0-9a-zA-Z$_]).*@.+$/.match(value)
+        # does not start with a quote, but contains a special character
+        raise(ArgumentError, "Database user #{value} must be properly quoted, invalid character: '#{matches[1]}'")
+      else
+        raise(ArgumentError, "Invalid database user #{value}")
       end
+
+      raise(ArgumentError, 'MySQL usernames are limited to a maximum of 16 characters') if user_part.size > 16
     end
 
     munge do |value|
-      user_part, host_part = value.split('@')
-      "#{user_part}@#{host_part.downcase}"
+      matches = /^((['`"]?).+\2)@([\w%\.:]+)$/.match(value)
+      "#{matches[1]}@#{matches[3].downcase}"
     end
   end
 
