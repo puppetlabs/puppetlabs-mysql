2088fb8b36ad2769e911dc2fedc0d88fe6a8004b
juniorsysadmin <juniorsysadmin@users.noreply.github.com>
2015-01-11 15:35:10 +1100

    Fix lint warning in server/service.pp

--- manifests/server/service.pp
+++ manifests/server/service.pp
@@ -20,7 +20,7 @@ class mysql::server::service {
     name     => $mysql::server::service_name,
     enable   => $mysql::server::real_service_enabled,
     provider => $mysql::server::service_provider,
-    require => [ File['mysql-config-file'], Package['mysql-server'] ]
+    require  => [ File['mysql-config-file'], Package['mysql-server'] ]
   }
 
 }
