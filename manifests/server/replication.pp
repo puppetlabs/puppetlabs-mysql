64b58ff4a0877d952d7cf8d1253f0c880be980c8
markasammut <mark.sammut@gamesysnetwork.com>
2015-07-02 16:22:59 +0200

    Update replication.pp

--- manifests/server/replication.pp
+++ manifests/server/replication.pp
@@ -1,7 +1,19 @@
 #
 class mysql::server::replication {
-  $server-id  = '1',
+  $master_slave  = $mysql::server::master_slave
   
-  
+  if $mysql::server::replication_enable {
+    exec { 'start-service':
+      command => '/etc/init.d/mysql start',
+      onlyif  => "test `grep -c rpl_semi_sync /usr/my.cnf` != 1",
+    }
+    
+    exec { 'install-replication-plugin':
+      command => "mysql -u root -e "INSTALL PLUGIN rpl_semi_sync_${mysql::server::master_slave} SONAME 'semisync_${mysql::server::master_slave}.so';"
+      path    => '/usr/bin:/bin',
+      onlyif  => "test `mysql -u root -e "show variables like 'rpl_semi_sync_${mysql::server::master_slave}_enabled';" 2>/dev/null | grep -c ON` != 1",
+      require => Exec['start-service'],
+    }
+  }
 
 }
