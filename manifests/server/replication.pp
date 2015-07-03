52bb3cece286425361aac23d21df8f58703f72e3
markasammut <mark.sammut@gamesysnetwork.com>
2015-07-03 11:01:03 +0200

    Update replication.pp

--- manifests/server/replication.pp
+++ manifests/server/replication.pp
@@ -1,28 +1,40 @@
 #
 class mysql::server::replication {
   $master_slave  = $mysql::server::master_slave
-  $options       = mysql_deepmerge($mysql::params::default_options, $mysql::server::override_options)
+  $server_id     = $mysql::server::rep_server_id
 
   if $mysql::server::replication_enable {
-
-    exec { 'initial-mysql-config-file':
-      command => "echo 'user=mysql' >> /usr/my.cnf; echo 'datadir=/home/mysql/data' >> /usr/my.cnf; echo 'socket=/home/mysql/data/mysql.sock' >> /usr/my.cnf; echo '[mysqld_safe]' >> /usr/my.cnf; echo 'log-error=/home/mysql/log/mysqld.log' >> /usr/my.cnf; echo 'pid-file=/home/mysql/run/mysqld/mysqld.pid' >> /usr/my.cnf",
-      unless  => "grep -c rpl_semi_sync /usr/my.cnf",
-      path    => '/usr/bin:/bin:/sbin',
-    }
-
-    exec { 'start-service':
-      command => '/etc/init.d/mysql start',
-      path    => '/usr/bin:/bin',
-      unless  => "grep -c rpl_semi_sync /usr/my.cnf",
-      require => Exec['initial-mysql-config-file'],
-    }
-
+    
     exec { 'install-replication-plugin':
       command => "mysql -u root -e \"INSTALL PLUGIN rpl_semi_sync_${mysql::server::master_slave} SONAME 'semisync_${mysql::server::master_slave}.so';\"",
-      path    => '/usr/bin:/bin',
+      path    => '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin',
       unless  => "mysql -u root -e \"show variables like 'rpl_semi_sync_${mysql::server::master_slave}_enabled';\" | grep rpl_semi | grep -c ON",
-      require => Exec['start-service'],
+    }
+
+    ini_setting { "server-id":
+      ensure  => present,
+      path    => '/usr/my.cnf',
+      section => 'mysqld',
+      setting => 'server-id',
+      value   => $server_id,
+      require => Exec['install-replication-plugin'],
+      notify  => Exec['restart-service'],
+    }
+    
+    ini_setting { "rpl_semi_sync_master":
+      ensure  => present,
+      path    => '/usr/my.cnf',
+      section => 'mysqld',
+      setting => "rpl_semi_sync_${master_slave}_enabled",
+      value   => '1',
+      require => Exec['install-replication-plugin'],
+      notify  => Exec['restart-service'],
+    }
+    
+    exec { 'restart-service':
+      command     => '/etc/init.d/mysql restart',
+      path        => '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin',
+      refreshonly => true,
     }
   }
 
