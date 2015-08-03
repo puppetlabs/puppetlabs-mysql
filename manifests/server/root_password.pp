b172ca02e2b6657ba5575f3b62efb52995a39315
Travis Fields <travis@puppetlabs.com>
2015-08-03 13:38:48 -0700

    (MODULES-2051) Suppress diff output for root password
    - ensure that we are not logging output of config file
      that contains sensitive password info

--- manifests/server/root_password.pp
+++ manifests/server/root_password.pp
@@ -17,6 +17,10 @@ class mysql::server::root_password {
       owner   => 'root',
       mode    => '0600',
     }
+    # show_diff was added with puppet 3.0
+    if versioncmp($::puppetversion, '3.0') <= 0 {
+      File["${::root_home}/.my.cnf"] { show_diff => false }
+    }
     if $mysql::server::create_root_user == true {
       Mysql_user['root@localhost'] -> File["${::root_home}/.my.cnf"]
     }
