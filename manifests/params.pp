67ea1c4dd3aaa4bcbd25e5259b4e57f557b4b88e
Hunter Haugen <hunter@puppet.com>
2016-04-11 14:49:18 -0700

    (MODULES-1256) Fix parameters on OpenSUSE 12

--- manifests/params.pp
+++ manifests/params.pp
@@ -96,7 +96,7 @@ class mysql::params {
     'Suse': {
       case $::operatingsystem {
         'OpenSuSE': {
-          if versioncmp( $::operatingsystemmajrelease, '13' ) >= 0 {
+          if versioncmp( $::operatingsystemmajrelease, '12' ) >= 0 {
             $client_package_name = 'mariadb-client'
             $server_package_name = 'mariadb'
             # First service start fails if this is set. Runs fine without
