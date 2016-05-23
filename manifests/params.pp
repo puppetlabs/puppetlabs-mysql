bf54a9259cf09d6e00c0ca892a23db5779313505
Guillaume <guillaume.roger@webpower.nl>
2016-05-23 08:51:28 +0200

    Use mariadb by default for Debian Jessie

--- manifests/params.pp
+++ manifests/params.pp
@@ -159,8 +159,19 @@ class mysql::params {
     }
 
     'Debian': {
-      $client_package_name     = 'mysql-client'
-      $server_package_name     = 'mysql-server'
+      if  $::lsbdistid == 'Debian' and $::lsbdistcodename == 'jessie' {
+        $client_package_name     = 'mariadb-client'
+        $server_package_name     = 'mariadb-server'
+
+        $client_dev_package_name = 'libmariadb-client-lgpl-dev'
+        $daemon_dev_package_name = 'libmariadb-dev'
+      } else {
+        $client_package_name     = 'mysql-client'
+        $server_package_name     = 'mysql-server'
+
+        $client_dev_package_name = 'libmysqlclient-dev'
+        $daemon_dev_package_name = 'libmysqld-dev'
+      }
 
       $basedir                 = '/usr'
       $config_file             = '/etc/mysql/my.cnf'
@@ -186,8 +197,6 @@ class mysql::params {
         'jessie'           => 'ruby-mysql',
         default            => 'libmysql-ruby',
       }
-      $client_dev_package_name = 'libmysqlclient-dev'
-      $daemon_dev_package_name = 'libmysqld-dev'
     }
 
     'Archlinux': {
