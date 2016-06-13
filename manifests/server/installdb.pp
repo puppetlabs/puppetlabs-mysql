89c96f9c2605d2abb20540b0a4b1b01ebcb65c50
Tom Murphy <t.murphy@ctidigital.com>
2016-06-13 11:16:44 +0100

    Remove log_error from mysql_datadir as it causes compilation and ruby errors on Puppet master

--- manifests/server/installdb.pp
+++ manifests/server/installdb.pp
@@ -31,7 +31,6 @@ class mysql::server::installdb {
       datadir             => $datadir,
       basedir             => $basedir,
       user                => $mysqluser,
-      log_error           => $log_error,
       defaults_extra_file => $_config_file,
     }
 
