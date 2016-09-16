ad259bd05a0190475fde02dacdc9a3d7b9621ec9
Dennis de Greef <dennisdegreef@users.noreply.github.com>
2016-09-16 12:30:21 +0200

    Silence STDERR in Facter (#883)
    * Silence STDERR in Facter
    
    * Alter test for previous change ( Silence STDERR in Facter)

--- spec/unit/facter/mysqld_version_spec.rb
+++ spec/unit/facter/mysqld_version_spec.rb
@@ -8,7 +8,7 @@ describe Facter::Util::Fact do
   describe "mysqld_version" do
     context 'with value' do
       before :each do
-        Facter::Util::Resolution.stubs(:exec).with('mysqld -V').returns('mysqld  Ver 5.5.49-37.9 for Linux on x86_64 (Percona Server (GPL), Release 37.9, Revision efa0073)')
+        Facter::Util::Resolution.stubs(:exec).with('mysqld -V 2>/dev/null').returns('mysqld  Ver 5.5.49-37.9 for Linux on x86_64 (Percona Server (GPL), Release 37.9, Revision efa0073)')
       end
       it {
         expect(Facter.fact(:mysqld_version).value).to eq('mysqld  Ver 5.5.49-37.9 for Linux on x86_64 (Percona Server (GPL), Release 37.9, Revision efa0073)')
