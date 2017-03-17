6af3240da41724efed3d4fd3e1deff31fd08c9cc
Joseph Karns <jmk61@psu.edu>
2017-03-17 14:11:18 -0400

    setting ip address as 127.0.0.1 instead of checking the loopback adapter

--- spec/unit/facter/mysql_server_id_spec.rb
+++ spec/unit/facter/mysql_server_id_spec.rb
@@ -17,7 +17,7 @@ describe Facter::Util::Fact do
 
     context "node with lo only" do
       before :each do
-        Facter.fact(:ipaddress_lo).stubs(:value).returns('172.0.0.1')
+        Facter.fact(:ipaddress).stubs(:value).returns('172.0.0.1')
       end
       it do
         expect(Facter.fact(:mysql_server_id).value).to eq(1515521)
