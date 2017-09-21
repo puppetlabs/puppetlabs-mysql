3e2274a07656d97fc9b9dc60b46fd84465ce1c55
Hailee Kenney <hailee@puppetlabs.com>
2017-09-21 14:57:45 -0700

    (FM-6362) Update language pack string
    Update the string passed to install_language_pack_on from 'ja_JP'
    to 'ja_JP.utf-8' so that it installs the correct package on the host.

--- spec/acceptance/locales_spec.rb
+++ spec/acceptance/locales_spec.rb
@@ -5,7 +5,7 @@ describe 'mysql localization', if: fact('osfamily') == 'Debian' do
   before :all do
     hosts.each do |host|
       on(host, "sed -i \"96i FastGettext.locale='ja'\" /opt/puppetlabs/puppet/lib/ruby/vendor_ruby/puppet.rb")
-      change_locale_on(host, 'ja_JP')
+      change_locale_on(host, 'ja_JP.utf-8')
     end
   end
 
