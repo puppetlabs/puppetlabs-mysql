7dbf3037ca476e481b87339966f37d3641648da1
Sebastien Badia <seb@sebian.fr>
2018-03-27 15:35:00 +0200

    (MODULES-6923) Migrate from puppet-staging to puppet-archive

--- spec/classes/mysql_server_mysqltuner_spec.rb
+++ spec/classes/mysql_server_mysqltuner_spec.rb
@@ -4,14 +4,13 @@ describe 'mysql::server::mysqltuner' do
   on_supported_os.each do |os, facts|
     context "on #{os}" do
       let(:facts) do
-        facts.merge(staging_http_get: 'curl',
-                    root_home: '/root')
+        facts.merge(root_home: '/root')
       end
 
       context 'ensure => present' do
         it { is_expected.to compile }
         it {
-          is_expected.to contain_staging__file('mysqltuner-v1.3.0').with(source: 'https://github.com/major/MySQLTuner-perl/raw/v1.3.0/mysqltuner.pl')
+          is_expected.to contain_archive('/usr/local/bin/mysqltuner').with(source: 'https://github.com/major/MySQLTuner-perl/raw/v1.3.0/mysqltuner.pl')
         }
       end
 
@@ -19,7 +18,7 @@ describe 'mysql::server::mysqltuner' do
         let(:params) { { ensure: 'absent' } }
 
         it { is_expected.to compile }
-        it { is_expected.to contain_file('/usr/local/bin/mysqltuner').with(ensure: 'absent') }
+        it { is_expected.to contain_archive('/usr/local/bin/mysqltuner').with(ensure: 'absent') }
       end
 
       context 'custom version' do
@@ -27,7 +26,7 @@ describe 'mysql::server::mysqltuner' do
 
         it { is_expected.to compile }
         it {
-          is_expected.to contain_staging__file('mysqltuner-v1.2.0').with(source: 'https://github.com/major/MySQLTuner-perl/raw/v1.2.0/mysqltuner.pl')
+          is_expected.to contain_archive('/usr/local/bin/mysqltuner').with(source: 'https://github.com/major/MySQLTuner-perl/raw/v1.2.0/mysqltuner.pl')
         }
       end
 
@@ -36,7 +35,7 @@ describe 'mysql::server::mysqltuner' do
 
         it { is_expected.to compile }
         it {
-          is_expected.to contain_staging__file('mysqltuner-/tmp/foo').with(source: '/tmp/foo')
+          is_expected.to contain_archive('/usr/local/bin/mysqltuner').with(source: '/tmp/foo')
         }
       end
     end
