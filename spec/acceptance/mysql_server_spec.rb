910b5db9eaeec0966f5e455ab5b5c4452ec5c5c8
David Swan <david.swan@puppet.co.uk>
2018-07-05 09:39:35 +0100

    no message

--- spec/acceptance/mysql_server_spec.rb
+++ spec/acceptance/mysql_server_spec.rb
@@ -2,85 +2,85 @@ require 'spec_helper_acceptance'
 require 'pry-byebug'
 
 describe 'mysql class' do
-  # describe 'advanced config' do
-  #   let(:pp) do
-  #     <<-MANIFEST
-  #       class { 'mysql::server':
-  #         manage_config_file      => 'true',
-  #         override_options        => { 'mysqld' => { 'key_buffer_size' => '32M' }},
-  #         package_ensure          => 'present',
-  #         purge_conf_dir          => 'true',
-  #         remove_default_accounts => 'true',
-  #         restart                 => 'true',
-  #         root_group              => 'root',
-  #         root_password           => 'test',
-  #         service_enabled         => 'true',
-  #         service_manage          => 'true',
-  #         users                   => {
-  #           'someuser@localhost' => {
-  #             ensure                   => 'present',
-  #             max_connections_per_hour => '0',
-  #             max_queries_per_hour     => '0',
-  #             max_updates_per_hour     => '0',
-  #             max_user_connections     => '0',
-  #             password_hash            => '*F3A2A51A9B0F2BE2468926B4132313728C250DBF',
-  #           }},
-  #         grants                  => {
-  #           'someuser@localhost/somedb.*' => {
-  #             ensure     => 'present',
-  #             options    => ['GRANT'],
-  #             privileges => ['SELECT', 'INSERT', 'UPDATE', 'DELETE'],
-  #             table      => 'somedb.*',
-  #             user       => 'someuser@localhost',
-  #           },
-  #         },
-  #         databases => {
-  #           'somedb' => {
-  #             ensure  => 'present',
-  #             charset => 'utf8',
-  #           },
-  #         }
-  #       }
-  #     MANIFEST
-  #   end
-
-  #   it_behaves_like 'a idempotent resource'
-  # end
-
-  describe 'minimal config' do
-    before(:all) do
-      @tmpdir = default.tmpdir('mysql')
-    end
-    # 'manage_config_file' being set to false can cause random failures in Debian 9
-    let(:manage_config_file) do
-      if fact('operatingsystem') == 'Debian' && fact('operatingsystemrelease') == '9'
-        'true'
-      else
-        'false'
-      end
-    end
+  describe 'advanced config' do
     let(:pp) do
       <<-MANIFEST
         class { 'mysql::server':
-          manage_config_file      => '#{manage_config_file}',
+          manage_config_file      => 'true',
           override_options        => { 'mysqld' => { 'key_buffer_size' => '32M' }},
           package_ensure          => 'present',
-          purge_conf_dir          => 'false',
-          remove_default_accounts => 'false',
-          restart                 => 'false',
-          root_group              => 'root',
+          purge_conf_dir          => 'true',
+          remove_default_accounts => 'true',
+          restart                 => 'true',
+          root_group              => 'root', 
           root_password           => 'test',
-          service_enabled         => 'false',
-          service_manage          => 'false',
-          users                   => {},
-          grants                  => {},
-          databases               => {},
+          service_enabled         => 'true',
+          service_manage          => 'true',
+          users                   => {
+            'someuser@localhost' => {
+              ensure                   => 'present',
+              max_connections_per_hour => '0',
+              max_queries_per_hour     => '0',
+              max_updates_per_hour     => '0',
+              max_user_connections     => '0',
+              password_hash            => '*F3A2A51A9B0F2BE2468926B4132313728C250DBF',
+            }},
+          grants                  => {
+            'someuser@localhost/somedb.*' => {
+              ensure     => 'present',
+              options    => ['GRANT'],
+              privileges => ['SELECT', 'INSERT', 'UPDATE', 'DELETE'],
+              table      => 'somedb.*',
+              user       => 'someuser@localhost',
+            },
+          },
+          databases => {
+            'somedb' => {
+              ensure  => 'present',
+              charset => 'utf8',
+            },
+          }
         }
       MANIFEST
     end
 
     it_behaves_like 'a idempotent resource'
   end
+
+  # describe 'minimal config' do
+  #   before(:all) do
+  #     @tmpdir = default.tmpdir('mysql')
+  #   end
+  #   # 'manage_config_file' being set to false can cause random failures in Debian 9
+  #   let(:manage_config_file) do
+  #     if fact('operatingsystem') == 'Debian' && fact('operatingsystemrelease') == '9'
+  #       'true'
+  #     else
+  #       'false'
+  #     end
+  #   end
+  #   let(:pp) do
+  #     <<-MANIFEST
+  #       class { 'mysql::server':
+  #         manage_config_file      => '#{manage_config_file}',
+  #         override_options        => { 'mysqld' => { 'key_buffer_size' => '32M' }},
+  #         package_ensure          => 'present',
+  #         purge_conf_dir          => 'false',
+  #         remove_default_accounts => 'false',
+  #         restart                 => 'false',
+  #         root_group              => 'root',
+  #         root_password           => 'test',
+  #         service_enabled         => 'false',
+  #         service_manage          => 'false',
+  #         users                   => {},
+  #         grants                  => {},
+  #         databases               => {},
+  #       }
+  #     MANIFEST
+  #   end
+
+  #   it_behaves_like 'a idempotent resource'
+  # end
 
   describe 'syslog configuration' do
     let(:pp) do
