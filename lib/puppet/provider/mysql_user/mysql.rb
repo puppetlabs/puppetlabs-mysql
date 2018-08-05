77801da5204e6df324f84e30ba0441be25c8a636
Andrew Ernst <ernstae@github.com>
2018-08-05 12:46:59 -0700

    Further linting cleanup to conform to style checks

--- lib/puppet/provider/mysql_user/mysql.rb
+++ lib/puppet/provider/mysql_user/mysql.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.join(File.dirname(__FILE__), '..', 'mysql'))
+
 Puppet::Type.type(:mysql_user).provide(:mysql, parent: Puppet::Provider::Mysql) do
   desc 'manage users for a mysql database.'
   commands mysql_raw: 'mysql'
@@ -69,7 +69,7 @@ Puppet::Type.type(:mysql_user).provide(:mysql, parent: Puppet::Provider::Mysql) 
       end
       @property_hash[:ensure] = :present
       @property_hash[:plugin] = plugin
-    elsif newer_than( 'mysql' => '8.0' )
+    elsif newer_than('mysql' => '8.0')
       self.class.mysql_caller("CREATE USER '#{merged_name}' IDENTIFIED WITH mysql_native_password AS '#{password_hash}'", 'system')
     else
       self.class.mysql_caller("CREATE USER '#{merged_name}' IDENTIFIED BY PASSWORD '#{password_hash}'", 'system')
@@ -77,7 +77,7 @@ Puppet::Type.type(:mysql_user).provide(:mysql, parent: Puppet::Provider::Mysql) 
       @property_hash[:password_hash] = password_hash
     end
 
-    if newer_than( 'mysql' => '8.0' )
+    if newer_than('mysql' => '8.0')
       self.class.mysql_caller("GRANT USAGE ON *.* TO '#{merged_name}'", 'system')
     else
       # rubocop:disable Metrics/LineLength
