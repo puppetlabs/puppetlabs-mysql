2d5aa0037c18b8d8e359233d85e4d0c25079bf30
Andreas Stuerz <andreas.stuerz@markt.de>
2020-03-10 17:54:50 +0100

    develop provider

--- lib/puppet/provider/mysql_login_path/mysql_login_path.rb
+++ lib/puppet/provider/mysql_login_path/mysql_login_path.rb
@@ -1,10 +1,40 @@
 # frozen_string_literal: true
 
 require 'puppet/resource_api/simple_provider'
+require 'puppet/util/execution'
+require 'json'
 
 # Implementation for the mysql_login_path type using the Resource API.
 class Puppet::Provider::MysqlLoginPath::MysqlLoginPath < Puppet::ResourceApi::SimpleProvider
+
+  def mysql_config_editor_cmd(context, *args)
+    args.unshift('/usr/bin/mysql_config_editor')
+    return Puppet::Util::Execution.execute(args, failonfail: true)
+  end
+
+  def my_print_defaults_cmd(context, *args)
+    args.unshift('/usr/bin/my_print_defaults')
+    return Puppet::Util::Execution.execute(args, failonfail: true)
+  end
+
+  def get_password_for_login_path(context, name)
+#    my_print_defaults -s local_tcp
+#    --user=root
+#    --password=test123
+#    --host=127.0.0.1
+#    --port=3306
+    result = my_print_defaults_cmd(context, '-s', name)
+    puts result
+  end
+
+  def list_login_paths(context)
+    result = mysql_config_editor_cmd(context, 'print', '--all')
+    puts result
+  end
+
   def get(context)
+    login_paths = list_login_paths(context)
+    puts "Hello"
     context.debug('Returning pre-canned example data')
     [
       {
--- scripts/run_single_tests.sh
+++ scripts/run_single_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #pdk test unit --tests=spec/unit/puppet/type/mysql_login_path_spec.rb
-pdk test unit --tests=spec/unit/puppet/provider/mysql_login_path/mysql_login_path_spec.rb
-#RSPEC_DEBUG=1 TARGET_HOST=localhost:2222 pdk bundle exec rspec ./spec/acceptance/types/mysql_login_path_spec.rb
+#pdk test unit --tests=spec/unit/puppet/provider/mysql_login_path/mysql_login_path_spec.rb
+RSPEC_DEBUG=1 TARGET_HOST=localhost:2222 pdk bundle exec rspec ./spec/acceptance/types/mysql_login_path_spec.rb
