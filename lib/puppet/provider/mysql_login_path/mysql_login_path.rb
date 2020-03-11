4adfb1f717c20e010d382d3e0f10e637efdd79f2
Andreas Stuerz <andreas.stuerz@markt.de>
2020-03-11 15:38:39 +0100

    develop provider

--- lib/puppet/provider/mysql_login_path/mysql_login_path.rb
+++ lib/puppet/provider/mysql_login_path/mysql_login_path.rb
@@ -7,9 +7,17 @@ require 'json'
 # Implementation for the mysql_login_path type using the Resource API.
 class Puppet::Provider::MysqlLoginPath::MysqlLoginPath < Puppet::ResourceApi::SimpleProvider
 
-  def mysql_config_editor_cmd(context, *args)
+  def get_homedir(context, uid)
+    result =  Puppet::Util::Execution.execute(['/usr/bin/getent', 'passwd', '1000'], failonfail: true)
+    return result.split(':')[5]
+  end
+
+  def mysql_config_editor_cmd(context, uid, *args)
     args.unshift('/usr/bin/mysql_config_editor')
-    return Puppet::Util::Execution.execute(args, failonfail: true)
+    homedir = get_homedir(context, uid)
+    return Puppet::Util::Execution.execute(args, failonfail: true, combine: true, uid: uid,
+                                           custom_environment: { 'HOME' => homedir }
+    )
   end
 
   def my_print_defaults_cmd(context, *args)
@@ -18,23 +26,24 @@ class Puppet::Provider::MysqlLoginPath::MysqlLoginPath < Puppet::ResourceApi::Si
   end
 
   def get_password_for_login_path(context, name)
-#    my_print_defaults -s local_tcp
-#    --user=root
-#    --password=test123
-#    --host=127.0.0.1
-#    --port=3306
     result = my_print_defaults_cmd(context, '-s', name)
     puts result
   end
 
-  def list_login_paths(context)
-    result = mysql_config_editor_cmd(context, 'print', '--all')
+  def list_login_paths(context, uid)
+    result = mysql_config_editor_cmd(context, uid, 'print', '--all')
+    puts "Command output list login paths"
     puts result
+    # parse to json
+    return []
+
   end
 
   def get(context)
-    login_paths = list_login_paths(context)
-    puts "Hello"
+    puts context
+    login_paths = list_login_paths(context, 0)
+    puts login_paths
+
     context.debug('Returning pre-canned example data')
     [
       {
--- spec/acceptance/types/mysql_login_path_spec.rb
+++ spec/acceptance/types/mysql_login_path_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper_acceptance'
 
+RSpec.configure do |c|
+  c.before(:each) do
+    Puppet::Util::Log.level = :debug
+    Puppet::Util::Log.newdestination(:console)
+  end
+end
+
 describe 'mysql_login_path' do
   describe 'setup' do
     pp = <<-MANIFEST
@@ -14,7 +21,6 @@ describe 'mysql_login_path' do
       class {'::mysql::client':
         package_name => 'mysql-community-client',
       }
-
     MANIFEST
     it 'works with no errors' do
       apply_manifest(pp, catch_failures: true)
@@ -22,24 +28,55 @@ describe 'mysql_login_path' do
   end
 
   context 'create login path with socket' do
-    describe 'create login path' do
+    describe 'add login path' do
       pp = <<-MANIFEST
         mysql_login_path { 'local_socket':
-          ensure => present,
+          host     => 'localhost',
+          user     => 'root',
+          password => 'secure',
+          socket   => '/var/run/mysql/mysql.sock',
+          ensure   => present,
         }
-
       MANIFEST
+      it 'works without errors' do
+        apply_manifest(pp, catch_failures: true)
+      end
+
+      #it 'finds the login path #stdout' do
+      #  run_shell("mysql_config_editor print -G local_socket") do |r|
+      #    expect(r.stdout).to match(%r{^\[local_socket\]\n})
+      #    expect(r.stdout).to match(%r{user = root\n})
+      #    expect(r.stdout).to match(%r{password = \*{5}\n})
+      #    expect(r.stdout).to match(%r{host = localhost\n})
+      #    expect(r.stderr).to be_empty
+      #  end
+      #end
+
     end
-
-
   end
-  # mysql_config_editor set --login-path=mysql1  --host=localhost \
-  #   --port=3306 --socket=/path/to/socket --user=root --password
-  #login-path                        client
-  #host                              FALSE
-  #password                          FALSE
-  #user                              FALSE
-  #warn                              TRUE
-  #socket                            FALSE
-  #port                              FALSE
+
+  #context 'create login path with socket' do
+  #  describe 'add login path' do
+  #    pp = <<-MANIFEST
+  #      mysql_login_path { 'local_socket':
+  #        host => 'localhost',
+  #        user => 'root',
+  #        password => 'secure',
+  #        socket => '/var/run/mysql/mysql.sock',
+  #        ensure => present,
+  #      }
+  #    MANIFEST
+  #    it 'works without errors' do
+  #      apply_manifest(pp, catch_failures: true)
+  #    end
+  #
+  #    it 'finds the login path #stdout' do
+  #      run_shell("mysql_config_editor print -G local_socket") do |r|
+  #        expect(r.stdout).to match(%r{^1$})
+  #        expect(r.stderr).to be_empty
+  #      end
+  #    end
+  #  end
+  #end
+
 end
