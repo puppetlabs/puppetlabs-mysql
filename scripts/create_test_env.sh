f60d41ad78928f274f877d0acd8fef1796ba60f6
Andreas Stuerz <andreas.stuerz@markt.de>
2020-03-25 16:47:22 +0100

    add support for debian/ubuntu

--- scripts/create_test_env.sh
+++ scripts/create_test_env.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-pdk bundle install
-pdk bundle exec rake 'litmus:provision[docker, litmusimage/centos:7]'
+#pdk bundle install
+#pdk bundle exec rake 'litmus:provision[docker, travis_deb]'
+pdk bundle exec rake 'litmus:provision_list[travis_deb]'
 pdk bundle exec rake litmus:install_agent
 pdk bundle exec rake litmus:install_module
