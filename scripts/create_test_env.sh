d07271c1a20d307e5d92d01310b6aab14e3c1a8c
Andreas Stuerz <andreas.stuerz@markt.de>
2020-03-27 11:02:08 +0100

    fix linter errors

--- scripts/create_test_env.sh
+++ scripts/create_test_env.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #pdk bundle install
-#pdk bundle exec rake 'litmus:provision_list[travis_deb]'
+pdk bundle exec rake 'litmus:provision_list[travis_deb]'
 #pdk bundle exec rake 'litmus:provision_list[travis_ub]'
 #pdk bundle exec rake 'litmus:provision_list[travis_el6]'
-pdk bundle exec rake 'litmus:provision_list[travis_el7]'
+#pdk bundle exec rake 'litmus:provision_list[travis_el7]'
 #pdk bundle exec rake 'litmus:provision_list[travis_el8]'
 pdk bundle exec rake litmus:install_agent
 pdk bundle exec rake litmus:install_module
