0b8a6ec11e7aa6184eaedd0c766d9000d85b4d71
Paula Muir <paula@puppet.com>
2021-01-07 15:38:55 +0000

    (IAC-1230) Add complex mysql db example

---
+++ examples/mysql_complex_db_grants_users.pp
@@ -0,0 +1,36 @@
+class { 'mysql::server':
+          manage_config_file      => 'true',
+          override_options        => { 'mysqld' => { 'key_buffer_size' => '32M' }},
+          package_ensure          => 'present',
+          purge_conf_dir          => 'true',
+          remove_default_accounts => 'true',
+          restart                 => 'true',
+          root_group              => 'root',
+          root_password           => 'test',
+          service_enabled         => 'true',
+          service_manage          => 'true',
+          users                   => {
+            'someuser@localhost' => {
+              ensure                   => 'present',
+              max_connections_per_hour => '0',
+              max_queries_per_hour     => '0',
+              max_updates_per_hour     => '0',
+              max_user_connections     => '0',
+              password_hash            => '*F3A2A51A9B0F2BE2468926B4132313728C250DBF',
+            }},
+          grants                  => {
+            'someuser@localhost/somedb.*' => {
+              ensure     => 'present',
+              options    => ['GRANT'],
+              privileges => ['SELECT', 'INSERT', 'UPDATE', 'DELETE'],
+              table      => 'somedb.*',
+              user       => 'someuser@localhost',
+            },
+          },
+          databases => {
+            'somedb' => {
+              ensure  => 'present',
+              charset => 'utf8',
+            },
+          }
+        }
