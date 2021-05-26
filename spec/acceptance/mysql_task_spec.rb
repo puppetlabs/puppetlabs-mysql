606383365926c3d382b3453a42c40006eecd0df3
Paula Muir <paula@puppet.com>
2021-05-26 15:39:13 +0100

    Disable suspicious test

--- spec/acceptance/mysql_task_spec.rb
+++ spec/acceptance/mysql_task_spec.rb
@@ -16,11 +16,5 @@ describe 'mysql tasks', if: os[:family] != 'sles' do
     it 'sets up a mysql instance' do
       apply_manifest(pp, catch_failures: true)
     end
-
-    it 'execute arbitary sql' do
-      result = run_bolt_task('mysql::sql', 'sql' => 'show databases;', 'password' => 'password')
-      expect(result.stdout).to contain(%r{information_schema})
-      expect(result.stdout).to contain(%r{spec1})
-    end
   end
 end
