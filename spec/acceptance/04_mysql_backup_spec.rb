0d1d6ccb167c63d6d8b981de98efd4bb050936b5
Garrett Honeycutt <code@garretthoneycutt.com>
2021-07-08 14:08:50 -0400

    Set ordering of acceptance tests
    This allows pre_run to be removed from the spec_helper_acceptance as the
    tests will run in a specific order. The pre_run just included the
    mysql::server class which now happens first thing.

--- spec/acceptance/04_mysql_backup_spec.rb
+++ spec/acceptance/04_mysql_backup_spec.rb
@@ -34,10 +34,6 @@ describe 'mysql::server::backup class' do
   end
 
   describe 'mysqlbackup.sh', if: Gem::Version.new(mysql_version) < Gem::Version.new('5.7.0') do
-    before(:all) do
-      pre_run
-    end
-
     it 'runs mysqlbackup.sh with no errors' do
       run_shell('/usr/local/sbin/mysqlbackup.sh') do |r|
         expect(r.stderr).to eq('')
@@ -99,10 +95,6 @@ describe 'mysql::server::backup class' do
     end
 
     describe 'mysqlbackup.sh', if: Gem::Version.new(mysql_version) < Gem::Version.new('5.7.0') do
-      before(:all) do
-        pre_run
-      end
-
       it 'runs mysqlbackup.sh with no errors without root credentials' do
         run_shell('HOME=/tmp/dontreadrootcredentials /usr/local/sbin/mysqlbackup.sh') do |r|
           expect(r.stderr).to eq('')
@@ -215,10 +207,6 @@ describe 'mysql::server::backup class' do
     end
 
     describe 'xtrabackup.sh', if: Gem::Version.new(mysql_version) < Gem::Version.new('5.7.0') && ((os[:family] == 'debian' && os[:release].to_i >= 9) || (os[:family] == 'ubuntu' && os[:release] =~ %r{^16\.04|^18\.04}) || (os[:family] == 'redhat' && os[:release].to_i > 6)) do # rubocop:disable Layout/LineLength
-      before(:all) do
-        pre_run
-      end
-
       it 'runs xtrabackup.sh full backup with no errors' do
         run_shell('/usr/local/sbin/xtrabackup.sh --target-dir=/tmp/xtrabackups/$(date +%F)_full --backup 2>&1 | tee /tmp/xtrabackup_full.log') do |r|
           expect(r.exit_code).to be_zero
@@ -332,10 +320,6 @@ describe 'mysql::server::backup class' do
     end
 
     describe 'xtrabackup.sh', if: Gem::Version.new(mysql_version) < Gem::Version.new('5.7.0') && ((os[:family] == 'debian' && os[:release].to_i >= 9) || (os[:family] == 'ubuntu' && os[:release] =~ %r{^16\.04|^18\.04}) || (os[:family] == 'redhat' && os[:release].to_i > 6)) do # rubocop:disable Layout/LineLength
-      before(:all) do
-        pre_run
-      end
-
       it 'runs xtrabackup.sh with no errors' do
         run_shell('/usr/local/sbin/xtrabackup.sh --target-dir=/tmp/xtrabackups/$(date +%F_%H-%M-%S) --backup 2>&1 | tee /tmp/xtrabackup.log') do |r|
           expect(r.exit_code).to be_zero
