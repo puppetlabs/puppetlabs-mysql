f76be35ab25ce4ca521c1378f0a58d77d582b70d
Lukas Audzevicius <laudzevicius24@hotmail.com>
2022-05-05 15:51:12 +0100

    syntax fix

--- spec/acceptance/00_mysql_server_spec.rb
+++ spec/acceptance/00_mysql_server_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require 'spec_helper_acceptance'
+
+# The export_locales method conflicts with our testing when the target system uses MariaDB, therefore it is being disabled for those systems.
 if os[:family] != 'debian'
   export_locales
 end
