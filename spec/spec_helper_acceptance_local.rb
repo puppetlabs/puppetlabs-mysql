3d1f894959c72f1f5bb51c6d3774c9156175c37c
Lukas Audzevicius <laudzevicius24@hotmail.com>
2022-05-20 15:10:43 +0100

    bash_profile?

--- spec/spec_helper_acceptance_local.rb
+++ spec/spec_helper_acceptance_local.rb
@@ -21,7 +21,7 @@ end
 
 def export_locales
   LitmusHelper.instance.run_shell('echo export PATH=/opt/puppetlabs/bin:$PATH > ~/.bashrc')
-  LitmusHelper.instance.run_shell('echo export PATH=/opt/puppetlabs/bin:$PATH > ~/.zshrc')
+  LitmusHelper.instance.run_shell('echo export PATH=/opt/puppetlabs/bin:$PATH > ~/.bash_profile')
   LitmusHelper.instance.run_shell('echo export LC_ALL=C > /etc/profile.d/my-custom.lang.sh')
   LitmusHelper.instance.run_shell('echo "## US English ##" >> /etc/profile.d/my-custom.lang.sh')
   LitmusHelper.instance.run_shell('echo export LANG=en_US.UTF-8 >> /etc/profile.d/my-custom.lang.sh')
@@ -30,9 +30,9 @@ def export_locales
   LitmusHelper.instance.run_shell('echo export LC_CTYPE=en_US.UTF-8 >> /etc/profile.d/my-custom.lang.sh')
   LitmusHelper.instance.run_shell('. /etc/profile.d/my-custom.lang.sh')
   LitmusHelper.instance.run_shell('echo export LC_ALL=C >> ~/.bashrc')
-  LitmusHelper.instance.run_shell('echo export LC_ALL=C >> ~/.zshrc')
+  LitmusHelper.instance.run_shell('echo export LC_ALL=C >> ~/.bash_profile')
   LitmusHelper.instance.run_shell('. ~/.bashrc')
-  LitmusHelper.instance.run_shell('. ~/.zshrc')
+  LitmusHelper.instance.run_shell('. ~/.bash_profile')
 end
 
 def fetch_charset
