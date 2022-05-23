96532319660b4a293a7ec593292d4a06d2fbf52f
Lukas Audzevicius <laudzevicius24@hotmail.com>
2022-05-23 15:18:21 +0100

    syntax

--- spec/acceptance/00_mysql_server_spec.rb
+++ spec/acceptance/00_mysql_server_spec.rb
@@ -68,7 +68,7 @@ describe 'mysql class' do
 
       it 'can be set' do
         apply_manifest(pp, catch_failures: true) do |r|
-          if os[:family] == 'debian' && os[:release].to_i > 9 && Puppet::Util::Package.versioncmp(Puppet.version, '7.0.0') < 0
+          if (((os[:family] == 'debian') && (os[:release].to_i > 9)) && Puppet::Util::Package.versioncmp(Puppet.version, '7.0.0') < 0)
             expect(r.stderr).to match(%r{locale environment variables were bad; continuing with LANG=C LC_ALL=C})
           else
             expect(r.stderr).to be_empty
