90b8c2632bf7fc6ad60cc51d61a02204a898d783
Craig Gumbley <craiggumbley@gmail.com>
2022-08-17 15:33:04 +0000

    Add spec tests
    This commit adds spec tests for the changes made in the previous
    commit.

--- spec/classes/mysql_server_spec.rb
+++ spec/classes/mysql_server_spec.rb
@@ -158,8 +158,8 @@ describe 'mysql::server' do
         describe 'when defaults' do
           it {
             is_expected.to contain_exec('remove install pass').with(
-              command: 'mysqladmin -u root --password=$(grep -o \'[^ ]\\+$\' /.mysql_secret) password \'\' && rm -f /.mysql_secret',
-              onlyif: 'test -f /.mysql_secret',
+              command: "mysqladmin -u root --password=\$(grep -o '[^ ]\\+\$' /.mysql_secret) password && (rm -f  /.mysql_secret; exit 0) || (rm -f /.mysql_secret; exit 1)",
+              onlyif: [['test', '-f', '/.mysql_secret']],
               path: '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin',
             )
           }
@@ -197,16 +197,6 @@ describe 'mysql::server' do
 
           it { is_expected.not_to contain_mysql_user('root@localhost') }
           it { is_expected.not_to contain_file('/root/.my.cnf') }
-        end
-        describe 'when install_secret_file set to /root/.mysql_secret' do
-          let(:params) { { install_secret_file: '/root/.mysql_secret' } }
-
-          it {
-            is_expected.to contain_exec('remove install pass').with(
-              command: 'mysqladmin -u root --password=$(grep -o \'[^ ]\\+$\' /root/.mysql_secret) password \'\' && rm -f /root/.mysql_secret',
-              onlyif: 'test -f /root/.mysql_secret',
-            )
-          }
         end
       end
 
