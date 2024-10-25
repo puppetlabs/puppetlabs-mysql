696c3e4eb410ce2605ea00ffe34dfe9843937022
Philippe BEAUMONT <philippe.beaumont@nfrance.com>
2024-10-25 15:12:37 +0200

    add path

--- lib/facter/mysqld_version.rb
+++ lib/facter/mysqld_version.rb
@@ -4,6 +4,6 @@ Facter.add('mysqld_version') do
   confine { Facter::Core::Execution.which('mysqld') || Facter::Core::Execution.which('/usr/libexec/mysqld') || Facter::Core::Execution.which('/usr/local/libexec/mysqld') }
   setcode do
     # Add /usr/libexec to PATH to find mysqld command
-    Facter::Core::Execution.execute('env PATH=$PATH:/usr/libexec mysqld --no-defaults -V 2>/dev/null')
+    Facter::Core::Execution.execute('env PATH=$PATH:/usr/libexec:/usr/local/libexec mysqld --no-defaults -V 2>/dev/null')
   end
 end
