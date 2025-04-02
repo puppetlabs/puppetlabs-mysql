7eb97b7c6fa40614cad61a28e11a5257e7ab4720
Mihmet Akpinar [CHECK24] <139950630+C24-AK@users.noreply.github.com>
2025-04-02 11:24:46 +0200

    Update lib/puppet/provider/mysql_user/mysql.rb
    Co-authored-by: renzBeltran24 <renz.beltran@check24.es>

--- lib/puppet/provider/mysql_user/mysql.rb
+++ lib/puppet/provider/mysql_user/mysql.rb
@@ -28,7 +28,7 @@ Puppet::Type.type(:mysql_user).provide(:mysql, parent: Puppet::Provider::Mysql) 
       if @plugin == 'caching_sha2_password'
         # Escaping all single quotes to prevent errors when password generated it
         @password = @password.gsub("'") { "\\'" }
-        @password = '0x' + @password.each_byte.map { |b| '%02X' % b.to_i }.join
+        @password = mysql_caller("SELECT CONCAT('0x',HEX('#{@password}'))", 'regular').chomp
       end
 
       @tls_options = parse_tls_options(ssl_type, ssl_cipher, x509_issuer, x509_subject)
